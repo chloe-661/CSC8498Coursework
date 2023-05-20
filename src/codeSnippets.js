2ad388120c6eab0ba6bd101559949d0c9c6f3884
chloe-661 <chloesunter@gmail.com>
2023-05-20 16:07:30 +0100

    ...

--- src/codeSnippets.js
+++ src/codeSnippets.js
@@ -96,10 +96,8 @@ const BuildTheHTMLForm__1 =
 	<div class="container">
 		<label for="user"><b>Username</b></label>
 		<input type="text" placeholder="Enter Username" name="user" required>
-
 		<label for="pass"><b>Password</b></label>
 		<input type="password" placeholder="Enter Password" name="pass" required>
-			
 		<button type="submit" id="login-form-submit" >Login</button>
 		<label>
 		<input type="checkbox" checked="checked" name="remember">
